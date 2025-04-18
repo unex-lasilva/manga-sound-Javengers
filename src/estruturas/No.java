53d513705845172543457907069ab8a5153cb484
João Vitor Bomfim <joaovitorbfm2004@gmail.com>
2025-04-18 02:38:21 -0300

    Reprodutor da playlist
    Implementação da funcionalidade de execução de listas de reprodução criadas pelo usuário. O código permite que o usuário selecione uma lista criada previamente e a execute utilizando o sistema de áudio baseado na classe SimpleAudioPlayer.
    
    O objetivo dessa modificação é permitir que os usuários possam ouvir as músicas das suas playlists diretamente pelo sistema, tornando a aplicação funcional como um player de música real. A execução utiliza a estrutura encadeada de listas, percorrendo a lista de músicas e tocando os arquivos .wav de forma sequencial.
    
    Como indicado na imagem abaixo), ao selecionar a opção 4:
    
    O sistema exibe todas as playlists já criadas, com seus respectivos índices.
    
    O usuário seleciona uma das playlists digitando o número correspondente.
    
    A lista escolhida é executada usando a classe ReprodutorLista, que percorre cada música e toca uma por vez.

---
+++ src/estruturas/No.java
@@ -0,0 +1,12 @@
+package estruturas;
+
+public class No<T> {
+    public T dado;
+    public No<T> proximo;
+    public Object valor;
+
+    public No(T dado) {
+        this.dado = dado;
+        this.proximo = null;
+    }
+}
