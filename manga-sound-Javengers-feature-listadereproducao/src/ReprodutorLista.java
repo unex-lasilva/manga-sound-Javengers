1c3bc657ad4c5678fbb3591c396ec56cc59d5f50
GUILHERME BRANDAO DE ARAUJO <guiba0093@gmail.com>
2025-04-18 02:09:58 -0300

    Add files via upload

---
+++ manga-sound-Javengers-feature-listadereproducao/src/ReprodutorLista.java
@@ -0,0 +1,33 @@
+public class ReprodutorLista {
+    private SimpleAudioPlayer player;
+
+    public void reproduzirLista(ListaReproducao lista) {
+        for (int i = 0; i < lista.getMusicas().getTamanho(); i++) {
+            Musica musica = lista.getMusicas().get(i);
+            System.out.println("▶ Tocando: " + musica);
+            try {
+                player = new SimpleAudioPlayer(musica.getPath());
+                player.play();
+                Thread.sleep(5000); // Simulação de 5 segundos
+                player.stop();
+            } catch (Exception e) {
+                System.out.println("Erro ao reproduzir: " + musica.getTitulo());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void pausar() {
+        if (player != null) player.pause();
+    }
+
+    public void executar() {
+        if (player != null) {
+            try {
+                player.resumeAudio();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
